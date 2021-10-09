46406d849650282a3ec6bf4c0daabd917f115da3
Tito <joshua1790@outlook.com>
2021-10-09 01:11:05 +0100

    changed debug value

--- JumbleServer.py
+++ JumbleServer.py
@@ -1,4 +1,6 @@
+from os import access
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -9,4 +11,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
